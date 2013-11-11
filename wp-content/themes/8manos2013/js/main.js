bc2f34b05136c76d832395a024ff6672c6487400
nicolazgarcia <nicolazgarcia@gmail.com>
2013-11-11 10:17:13 -0500

    test equipo-detalle basic | contacto basic

--- wp-content/themes/8manos2013/js/main.js
+++ wp-content/themes/8manos2013/js/main.js
@@ -1,7 +1,7 @@
 (function($) {
 	var items = $('.slides li').width();	  
 	$('.gallery').flexslider({
-		animation: "slide",
+		animation: 'slide',
 		slideshow: false,
 		controlNav: false,
 		directionNav: false,
@@ -13,12 +13,28 @@
 	
 	
 /////// imagenes equipo
-	var imageUrl = $('.thumbnail').find('img').attr('src');
-	$('.thumbnail').css('background-image', 'url("' + imageUrl + '")');
+	
+	$('.thumbnail').each(function(){
+	    var imageUrl = $(this).find('img').attr('src');
+		$(this).css('background-image', 'url("' + imageUrl + '")');
+	});
 
 /////// mosaico laboratorio
 	$('#mosaic').freetile({
 		selector: 'article'
+	});
+	
+/////// tabs
+	$('.nav-tabs li:first').addClass('active').show();
+	$('.content.tabs:first').show();
+
+	$('.nav-tabs li').click(function() {
+		$('.nav-tabs li').removeClass('active');
+		$(this).addClass('active');
+		$('.content.tabs').hide();
+		var activeTab = $(this).find('a').attr('href');
+		$(activeTab).fadeIn();
+		return false;
 	});
 	
 })(jQuery);
