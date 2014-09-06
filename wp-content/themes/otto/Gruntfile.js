25490413d896d84f55746e9e541ae47d4586f194
Anabelle Handdoek <github@huellaspyp.com>
2014-09-06 18:03:21 -0500

    agrega imagemin

--- wp-content/themes/otto/Gruntfile.js
+++ wp-content/themes/otto/Gruntfile.js
@@ -27,16 +27,38 @@ module.exports = function(grunt) {
 			},
 		},
 
+		// image optimization
+		imagemin: {
+			dist: {
+				options: {
+					optimizationLevel: 7,
+					progressive: true,
+					interlaced: true
+				},
+				files: [{
+					expand: true,
+					cwd: 'img/',
+					src: ['**/*.{png,jpg,gif}'],
+					dest: 'img/'
+				}]
+			}
+		},
+
 		watch: {
 			css: {
 				files: '**/*.scss',
 				tasks: ['compass', 'autoprefixer']
+			},
+			images: {
+				files: ['img/*.{png,jpg,gif}'],
+				tasks: ['imagemin']
 			}
 		}
 	});
 	grunt.loadNpmTasks('grunt-autoprefixer');
 	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.registerTask('default',['compass', 'autoprefixer', 'watch']);
+	grunt.registerTask('default',['compass', 'autoprefixer', 'imagemin', 'watch']);
 }
