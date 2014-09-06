1b165a5149c913bc32e0b35237f5f55e19dddb94
Anabelle Handdoek <github@huellaspyp.com>
2014-09-06 17:55:17 -0500

    compass en lugar de sass

--- wp-content/themes/otto/Gruntfile.js
+++ wp-content/themes/otto/Gruntfile.js
@@ -1,37 +1,42 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		sass: {
-			dist: {
-				files: {
-					'css/styles.css' : 'sass/style.scss'
+
+		// compass 
+		compass: { 
+			dev: {
+				options: {
+					sassDir: 'sass',
+					cssDir: 'css',
+					outputStyle: 'nested'
 				}
 			}
 		},
 
 		// autoprefixer
-        autoprefixer: {
-            options: {
-                browsers: ['last 2 versions', 'ie 9', 'ios 6', 'android 4'],
-                map: true
-            },
-            files: {
-                expand: true,
-                flatten: true,
-                src: 'css/*.css',
-                dest: 'css'
-            },
-        },
+		autoprefixer: {
+			options: {
+				browsers: ['last 2 versions', 'ie 9', 'ios 6', 'android 4'],
+				map: true
+			},
+			files: {
+				expand: true,
+				flatten: true,
+				src: 'css/*.css',
+				dest: 'css'
+			},
+		},
 
 		watch: {
 			css: {
 				files: '**/*.scss',
-				tasks: ['sass', 'autoprefixer']
+				tasks: ['compass', 'autoprefixer']
 			}
 		}
 	});
 	grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.loadNpmTasks('grunt-contrib-compass');
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.registerTask('default',['sass', 'autoprefixer', 'watch']);
+	grunt.registerTask('default',['compass', 'autoprefixer', 'watch']);
 }
