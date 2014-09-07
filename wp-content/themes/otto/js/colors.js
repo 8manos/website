17ccf5ab5f6aeef3fbbeb5bc05491ee134aea1fe
Anabelle Handdoek <github@huellaspyp.com>
2014-09-06 22:46:32 -0500

    hsl and rgb color setting test

--- wp-content/themes/otto/js/colors.js
+++ wp-content/themes/otto/js/colors.js
@@ -15,31 +15,35 @@
 		var g = changeRangeTo255( y ); // 0, 255
 		var b = changeRangeTo255( z ); // 0, 255
 
-		$('#data-r').text( Math.floor(r) );
-		$('#data-g').text( Math.floor(g) );
-		$('#data-b').text( Math.floor(b) );
+		$('#data-r').text( r );
+		$('#data-g').text( g );
+		$('#data-b').text( b );
 
 		// HSL
-		var h = x; // 0, 360
+		var h = Math.floor( x ); // 0, 360
 		var s = changeRangeTo100( y ); // 0, 100
 		var l = changeRangeTo100( z ); // 0, 100
 
-		$('#data-h').text( Math.floor(h) );
-		$('#data-s').text( Math.floor(s) );
-		$('#data-l').text( Math.floor(l) );
+		$('#data-h').text( h );
+		$('#data-s').text( s );
+		$('#data-l').text( l );
+
+		// Pruebas de setting de color
+		$( 'header' ).css( 'background-color', 'rgb('+r+','+g+','+b+')' );
+		$( 'body' ).css( 'background-color', 'hsl('+h+','+s+'%,'+l+'%)' );
 
 		window.console && console.info('Raw Position: x, y, z: ', x, y, z);
 	}
 
 	function changeRangeTo255( val ){
 		var converted = ( val * 255 ) / 360;
-		return converted;
+		return Math.floor( converted );
 	}
 
 	function changeRangeTo100( val ){
 		var converted = ( val * 100 ) / 360;
-		return converted;
+		return Math.floor( converted );
 	}
 
-	window.addEventListener("deviceorientation", handleEvent, true);	
+	window.addEventListener( 'deviceorientation' , handleEvent, true );	
 })(jQuery);
