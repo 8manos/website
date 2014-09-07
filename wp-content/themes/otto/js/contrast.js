9be64c42db2f6c8b9c32e96cbcfa3980ce17d33a
Carlos Sosa <cesosag@gmail.com>
2014-09-07 11:56:09 -0500

    Device light test

--- wp-content/themes/otto/js/contrast.js
+++ wp-content/themes/otto/js/contrast.js
@@ -1,7 +1,7 @@
 (function($){
 	window.addEventListener('devicelight', function(e){
 		var lux = e.value;
-
+		$("#lux-level").text(lux);
 		/*if (lux < 50) { // luz tenue
 	    	document.body.className = 'tenue';
 		}
