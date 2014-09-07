e7d38cc794fc409a35e09f03c68427d44ec1c722
Carlos Sosa <cesosag@gmail.com>
2014-09-07 00:52:09 -0500

    Team view

--- wp-content/themes/otto/js/routers.js
+++ wp-content/themes/otto/js/routers.js
@@ -1,6 +1,7 @@
 OM.Routers.Router = Backbone.Router.extend({
   routes: {
     '': 'root',
+    'equipo': 'team'
   },
   root: function(){
     var homeCollection = new OM.Collections.PageCollection();
@@ -11,5 +12,15 @@ OM.Routers.Router = Backbone.Router.extend({
         }
       },
     });
+  },
+  team: function(){
+    var teamCollection = new OM.Collections.TeamCollection();
+    teamCollection.fetch({
+      complete: function(xhr, textStatus){
+        if(textStatus == 'success'){
+          window.views.person_view = new OM.Views.PersonView({collection: teamCollection});
+        }
+      }
+    })
   }
 });
