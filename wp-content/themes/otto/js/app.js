5ea630454a0773bfb4df92584e1bdaea6f8a968a
MaoMantish <mauricio@8manos.com>
2014-09-08 16:50:16 -0500

    add ajax navigation for wordpress menu

--- wp-content/themes/otto/js/app.js
+++ wp-content/themes/otto/js/app.js
@@ -1,19 +1,41 @@
 $(document).on('ready', function(){
+
   var menuUrls = [];
+  var pages = [];
   var homeUrl = $('h1 a[rel="home"]').attr('href');
-  $('#menu-main .menu-item a').each(function() {
-    var linkUrl = $(this).attr('href');
-    var linkUrlArray = linkUrl.split(homeUrl);
+
+  function urlPath(link) {
+    //get relative url
+    var linkUrlArray = link.split(homeUrl);
     var linkPath = linkUrlArray.length > 1 ? linkUrlArray[1] : linkUrlArray[0];
-    linkPath = linkPath.replace(/(^\/+|\/+$)/g,''); //trim starting and final slash
+    return cleanUrl(linkPath);
+  }
+
+  function cleanUrl(url) {
+    url = url.replace(/(^\/+|\/+$)/g,''); //trim starting and trailing slash
+    if (url.length > 0) {//if not home, add trailing slash
+      url += '/';
+    }
+    return url;
+  }
+
+  $('#menu-main .menu-item a').each(function() {
+    var href = $(this).attr('href');
+    var linkPath = urlPath(href);
     menuUrls.push(linkPath);
   });
 
-  var pages = [];
   $('#menu-main .menu-item-object-page').each(function() {
     var pageClasses = $(this).attr('class');
     var pageId = pageClasses.split('page-id-')[1];
     pages.push(pageId);
+  });
+
+  $('#menu-main .menu-item a').on('click', function(e) {
+    e.preventDefault();
+    var href = $(this).attr('href');
+    var linkPath = urlPath(href);
+    Backbone.history.navigate(linkPath, {trigger: true});
   });
 
   window.routers.router = new OM.Routers.Router({pages: pages, urls: menuUrls});
