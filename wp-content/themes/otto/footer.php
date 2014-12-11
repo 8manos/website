5244dfb6093e00e948f4d291c3977ff98d9d91d6
mantish <mauricio@8manos.com>
2014-12-11 17:57:14 -0500

    test ugly fix for ios

--- wp-content/themes/otto/footer.php
+++ wp-content/themes/otto/footer.php
@@ -5,6 +5,8 @@
 		</div>
 	</div>
 
+	<div class="ugly-filler"></div>
+
 	<footer role="contentinfo" class="contact-footer">
 		<h3>Aca va un texto introductorio que se administra como una página en wordpress. O podemos poner un texto relacionado con la sección en la que uno se encuentra?</h3>
 		<p>Prometemos responder en el menor tiempo posible.</p>
