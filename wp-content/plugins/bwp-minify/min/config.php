391f4419207048699adb880f70737ec7d8b94ed2
8manos <git@8manos.com>
2015-02-09 15:34:58 -0600

    server version

--- wp-content/plugins/bwp-minify/min/config.php
+++ wp-content/plugins/bwp-minify/min/config.php
@@ -13,4 +13,4 @@ $min_symlinks = array();
 $min_uploaderHoursBehind = 0;
 $min_libPath = dirname(__FILE__) . '/lib';
 ini_set('zlib.output_compression', '0');
-// auto-generated on 2015-02-09 12:19:28
+// auto-generated on 2015-02-09 13:36:27
